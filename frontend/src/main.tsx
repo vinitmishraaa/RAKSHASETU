04f00d4efd77147d3ab93b31019a9631e6db9187
Vinit Mishra <mishravinit923@gmail.com>
2026-09-06 00:59:05 +0530

    ui: load assistant desk styling

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import "./module-polish.css";
+import "./pages/Assistant/assistant.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
