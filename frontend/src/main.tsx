3bdcfe6b83ec422202f58f91914aae247c86b460
Vinit Mishra <mishravinit923@gmail.com>
2026-09-17 13:13:09 +0530

    feat: enable light theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import "./light-theme.css";
 import "./module-polish.css";
 import "./pages/Assistant/assistant.css";
 
